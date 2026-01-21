da7db6b5119281f19394dd716085140f8ca5ae84
unitycom25 <unitycom25@gmail.com>
2026-01-21 18:37:33 +0200

    ф

---
+++ app/src/main/java/com/protivost/game/MainActivity.java
@@ -0,0 +1,19 @@
+package com.protivost.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://protivost.top");
+    }
+}
